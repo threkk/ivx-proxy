d3d1bf374251f5df0372c2c18f7f98950c5c8c06
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2021-11-23 14:05:50 +0900

    feat: download works again after changing the logic for finding the audio file

--- internal/app/download.go
+++ internal/app/download.go
@@ -1,69 +1,47 @@
 package app
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"regexp"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
-func getURLandFindOne(url string, re *regexp.Regexp) (string, error) {
-	if !isValidURL(url) {
-		return "", fmt.Errorf("the parameter provided is not a URL: %s", url)
-	}
+const (
+	IVOOX_AUDIO_ML    = "http://www.ivoox.com/listen_mn_id_1.m4a?internal=HTML5"
+	IVOOX_AUDIO_ID_RE = `(?m)var\s+idaudio\s+=\s+(\d+)`
+)
 
-	res, err := http.Get(url)
-	if err != nil || res.StatusCode != 200 {
-		return "", fmt.Errorf("error requesting the remote resource: %s", url)
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", fmt.Errorf("error reading the remote resource: %s", url)
-	}
-
-	elements := re.FindSubmatch(body)
-	if len(elements) != 2 {
-		return "", fmt.Errorf("element not found")
-	}
-
-	return string(elements[1]), nil
-}
+var reAudio = regexp.MustCompile(IVOOX_AUDIO_ID_RE)
 
 func (a *App) handleDownload() http.HandlerFunc {
-	reDownload := regexp.MustCompile(`\$\('\.downloadlink'\)\.load\('(.+)'\)`)
-	reFile := regexp.MustCompile(`downloadFollow\(event,'(.+)'\);`)
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 
-		link, err := getURLandFindOne(vars["url"], reDownload)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Header().Add("Content-Type", "application/json; charset=utf-8")
-			json.NewEncoder(w).Encode(errorResponse{
-				Status:        400,
-				Code:      "Bad Request",
-				Message: err.Error(),
-			})
+		if url, ok := vars["url"]; !ok || !isValidURL(url) {
+			a.HandleError(w, http.StatusUnprocessableEntity, "The parameter provided is missing or not a URL")
 			return
 		}
 
-		file, err := getURLandFindOne("https://www.ivoox.com/"+link, reFile)
+		entry, err := fetch(vars["url"])
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Header().Add("Content-Type", "application/json; charset=utf-8")
-			json.NewEncoder(w).Encode(errorResponse{
-				Status:        400,
-				Code:      "Bad Request",
-				Message: err.Error(),
-			})
+			a.Err(err.Error())
+			a.HandleError(w, http.StatusBadRequest, "The URL could not be retrieved")
 			return
 		}
+
+		matches := reAudio.FindStringSubmatch(entry)
+		if matches == nil {
+			a.Err(fmt.Sprintf("File not found in the entry. url=%s", vars["url"]))
+			a.HandleError(w, http.StatusInternalServerError, "File not found in the entry")
+			return
+		}
+
+		audioId := matches[1]
+		// Believe it or not, this is how it works in the original version
+		file := strings.Replace(IVOOX_AUDIO_ML, "id", audioId, 1)
 
 		http.Redirect(w, r, file, http.StatusMovedPermanently)
 	}
